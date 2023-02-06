9495f9123c9ba55f042790893ea089f0c846d7a3
aleksej <dr1ma@icloud.com>
2023-02-06 18:50:32 +0600

    1

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+class Main
+{
+    public static<Temp> List<Temp> reverseList(List<Temp> list)
+    {
+        List<Temp> reverse = new ArrayList<>(list.size());//временный списолк
+
+        new LinkedList<>(list)
+                .descendingIterator() //обратный последовательный поток
+                .forEachRemaining(reverse::add);
+
+        return reverse;
+    }
+
+    public static void main(String[] args)
+    {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+
+        List<Integer> reverse = reverseList(list);
+        System.out.println(reverse);
+    }
+}
